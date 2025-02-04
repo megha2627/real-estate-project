1909b2bd906f284ed647074754239e01eb35c933
megha <meghagupta270505@gmail.com>
2025-02-04 10:46:38 +0530

    adding user route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,19 @@
 import bcrypt from 'bcrypt';
+import prisma from "../lib/prisma.js";
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
+
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword
+        },
+    })
+    console.log(newUser);
+    res.status(201).json({message: 'User created successfully'})
     
 }
 
