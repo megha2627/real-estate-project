d004f6450db2606350f82c80ab6288ba3c5cb794
megha <meghagupta270505@gmail.com>
2025-02-17 18:46:21 +0530

    adding login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
 import jwt from "jsonwebtoken";
+
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
@@ -17,39 +18,46 @@ export const register = async (req, res) => {
     console.log(newUser);
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
+    if (error.code === 'P2002' && error.meta.target === 'User_email_key') {
+      res.status(409).json({ message: "Email already exists" });
+    } else {
       console.log(error);
       res.status(500).json({ message: "Failed to create user" });
+    }
   }
 };
 
-export const login =async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = await prisma.user.findUnique({ where: { username } })
-        if (!user) {
-             return res.status(401).json({message:"Invalid credentials"});
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password)
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Invalid credentials" });
-      }
-       const age = 1000 * 60 * 60 * 24 * 7;
-      const token = jwt.sign({
-        id:user.id
-      },process.env.JWT_SECRET_KEY,{expiresIn:age});
-       
-        res.cookie("token", token, {
-            httpOnly: true,
-            maxAge: age
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
 
-           
-       }).status(200).json({ message: 'login successful'})
-        //res.setHeader("Set-Cookie","test="+"myValue").json({message:"success"})
-    }
-    catch {
-        console.log(err);
-        res.status(500).json({ message: "Failed to login user" });
-    }
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "login successful" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login user" });
+  }
 };
 
 export const logout = (req, res) => {
